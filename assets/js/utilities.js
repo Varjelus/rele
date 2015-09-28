1f37fe29e4958274d02509ef240a03690486bd64
Timo Virkkunen <timo@comsec.ninja>
2015-09-29 02:29:25 +0300

    Remove old, problematic linkifying methods

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -8,17 +8,6 @@ function msToTime(ms) {
 	return h + ":" + m;
 }
 
-function linkify(str) {
-	var e = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gi,
-	t = /(^|[^\/])(www\.[\S]+(\b|$))/gi,
-	n = /\w+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gi;
-	return str.replace(e, '<a target="_blank" href="$&">$&</a>').replace(t, '$1<a target="_blank" href="http://$2">$2</a>').replace(n, '<a target="_blank" href="mailto:$&">$&</a>');
-}
-
-function makeSafe(unsafe) {
-	return unsafe.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
-}
-
 function cleanUp() {
 	this.hide()
 	tray.remove()
